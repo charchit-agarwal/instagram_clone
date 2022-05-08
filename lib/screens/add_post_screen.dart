f3007fa40db5f27c586f6fc00e4dd0aeccf66056
charchit-agarwal <charchit957@gmail.com>
2022-05-08 23:11:52 +0530

    Selecting Image Done

--- lib/screens/add_post_screen.dart
+++ lib/screens/add_post_screen.dart
@@ -1,5 +1,12 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:instagram_clone/models/user.dart';
+import 'package:instagram_clone/provider/user_provider.dart';
 import 'package:instagram_clone/utils/colors.dart';
+import 'package:instagram_clone/utils/utils.dart';
+import 'package:provider/provider.dart';
 
 class AddPostScreen extends StatefulWidget {
   const AddPostScreen({Key? key}) : super(key: key);
@@ -9,80 +16,126 @@ class AddPostScreen extends StatefulWidget {
 }
 
 class _AddPostScreenState extends State<AddPostScreen> {
+  Uint8List? _file;
+  final TextEditingController _descriptionController = TextEditingController();
+  _selectImage(BuildContext context) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return SimpleDialog(
+            title: Text('Create a Post'),
+            children: [
+              SimpleDialogOption(
+                padding: EdgeInsets.all(20),
+                child: Text('Take a photo'),
+                onPressed: () async {
+                  Navigator.of(context).pop();
+                  Uint8List file = await pickImage(ImageSource.camera);
+                  setState(() {
+                    _file = file;
+                  });
+                },
+              ),
+              SimpleDialogOption(
+                padding: EdgeInsets.all(20),
+                child: Text('Choose from gallery'),
+                onPressed: () async {
+                  Navigator.of(context).pop();
+                  Uint8List file = await pickImage(ImageSource.gallery);
+                  setState(() {
+                    _file = file;
+                  });
+                },
+              ),
+              SimpleDialogOption(
+                padding: EdgeInsets.all(20),
+                child: Text('Cancel'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
-    // return Center(
-    //   child: IconButton(
-    //     icon: Icon(
-    //       Icons.upload,
-    //       size: 30,
-    //     ),
-    //     onPressed: () {},
-    //   ),
-    // );
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: mobileBackgroundColor,
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back),
-            onPressed: () {},
-          ),
-          title: const Text('Post to'),
-          actions: [
-            TextButton(
-              onPressed: () {},
-              child: const Text(
-                'Post',
-                style: TextStyle(
-                    fontSize: 24,
-                    color: Colors.blueAccent,
-                    fontWeight: FontWeight.bold),
+    final User user = Provider.of<UserProvider>(context).getUser;
+    return _file == null
+        ? Center(
+            child: IconButton(
+              icon: Icon(
+                Icons.upload,
+                size: 30,
               ),
-            )
-          ],
-        ),
-        body: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                CircleAvatar(
-                  backgroundImage: NetworkImage(
-                      'https://www.tenforums.com/geek/gars/images/2/types/thumb_15951118880user.png'),
+              onPressed: () {
+                _selectImage(context);
+              },
+            ),
+          )
+        : SafeArea(
+            child: Scaffold(
+              appBar: AppBar(
+                backgroundColor: mobileBackgroundColor,
+                leading: IconButton(
+                  icon: Icon(Icons.arrow_back),
+                  onPressed: () {},
                 ),
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.45,
-                  child: TextField(
-                    decoration: InputDecoration(
-                        hintText: 'Write a caption...',
-                        border: InputBorder.none),
-                    maxLines: 8,
-                  ),
-                ),
-                SizedBox(
-                  height: 45,
-                  width: 45,
-                  child: AspectRatio(
-                    aspectRatio: 487 / 451,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        image: DecorationImage(
-                          image: NetworkImage(
-                              'https://www.tenforums.com/geek/gars/images/2/types/thumb_15951118880user.png'),
-                          fit: BoxFit.fill,
-                          alignment: FractionalOffset.topCenter,
+                title: const Text('Post to'),
+                actions: [
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'Post',
+                      style: TextStyle(
+                          fontSize: 24,
+                          color: Colors.blueAccent,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  )
+                ],
+              ),
+              body: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      CircleAvatar(
+                        backgroundImage: NetworkImage(user.profileURL),
+                      ),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.45,
+                        child: TextField(
+                          controller: _descriptionController,
+                          decoration: InputDecoration(
+                              hintText: 'Write a caption...',
+                              border: InputBorder.none),
+                          maxLines: 8,
                         ),
                       ),
-                    ),
-                  ),
-                )
-              ],
-            )
-          ],
-        ),
-      ),
-    );
+                      SizedBox(
+                        height: 45,
+                        width: 45,
+                        child: AspectRatio(
+                          aspectRatio: 487 / 451,
+                          child: Container(
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                image: MemoryImage(_file!),
+                                fit: BoxFit.fill,
+                                alignment: FractionalOffset.topCenter,
+                              ),
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          );
   }
 }
